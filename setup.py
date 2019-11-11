c373a042171a7c13b4ec60295cf363c681a04661
Pablo Marti <pablo@getdrop.com>
2019-11-11 15:06:18 +0100

    bump version to avoid problems with last change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Datadog',
-    version='0.1.8',
+    version='0.1.9',
     url='https://github.com/50onRed/flask-datadog.git',
     license='BSD',
     author='50onRed',
